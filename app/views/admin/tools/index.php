6fd51c5d835681031ca5f4bf4dafe95c232dca11
szami <shabirin82@gmail.com>
2026-01-11 21:16:29 +0800

    feat: Restore all missing System Tools features
    - Added Email System section (Templates, Reminders, Config)
    - Added Sync Guide to Database Tools
    - Added Card Design to System Configuration (using existing /admin/exam-card/design route)
    - Added User & Maintenance section (Deploy, Update) for superadmin
    - System Tools now matches production server with 10 complete features
    - All routes verified and functional

--- app/views/admin/tools/index.php
+++ app/views/admin/tools/index.php
@@ -33,18 +33,92 @@
                                 class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
+
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>Sync Guide</h3>
+                            <p>Sinkronisasi Eksternal</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-sync"></i>
+                        </div>
+                        <a href="/admin/sync-guide" class="small-box-footer">View Guide <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
             </div>
         <?php endif; ?>
 
+        <?php if (\App\Utils\RoleHelper::canManageEmail()): ?>
+            <h5 class="mt-4 mb-2"><i class="fas fa-envelope mr-1"></i> Email System</h5>
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-purple">
+                        <div class="inner">
+                            <h3>Templates</h3>
+                            <p>Kelola Template Email</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-file-alt"></i>
+                        </div>
+                        <a href="/admin/email/templates" class="small-box-footer">Manage Templates <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-purple">
+                        <div class="inner">
+                            <h3>Reminders</h3>
+                            <p>Kirim & Log Reminder</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-paper-plane"></i>
+                        </div>
+                        <a href="/admin/email/reminders" class="small-box-footer">Go to Reminders <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-secondary">
+                        <div class="inner">
+                            <h3>Config</h3>
+                            <p>Konfigurasi Email/SMTP</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-cog"></i>
+                        </div>
+                        <a href="/admin/email/config" class="small-box-footer">Configure <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
 
         <?php if (\App\Utils\RoleHelper::canManageSettings()): ?>
             <h5 class="mt-4 mb-2"><i class="fas fa-cogs mr-1"></i> System Configuration</h5>
             <div class="row">
                 <div class="col-lg-3 col-6">
-                    <div class="small-box bg-warning">
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>Card Design</h3>
+                            <p>Desain Kartu Ujian</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-id-card"></i>
+                        </div>
+                        <a href="/admin/exam-card/design" class="small-box-footer">Edit Design <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-dark">
                         <div class="inner">
                             <h3>Settings</h3>
-                            <p>System Settings</p>
+                            <p>Pengaturan Sistem Utama</p>
                         </div>
                         <div class="icon">
                             <i class="fas fa-cog"></i>
@@ -56,10 +130,10 @@
 
                 <?php if (\App\Utils\RoleHelper::isSuperadmin()): ?>
                     <div class="col-lg-3 col-6">
-                        <div class="small-box bg-danger">
+                        <div class="small-box bg-secondary">
                             <div class="inner">
                                 <h3>Users</h3>
-                                <p>User Management</p>
+                                <p>Manajemen User Admin</p>
                             </div>
                             <div class="icon">
                                 <i class="fas fa-users-cog"></i>
@@ -69,6 +143,39 @@
                         </div>
                     </div>
                 <?php endif; ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if (\App\Utils\RoleHelper::isSuperadmin()): ?>
+            <h5 class="mt-4 mb-2"><i class="fas fa-tools mr-1"></i> User & Maintenance</h5>
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-purple">
+                        <div class="inner">
+                            <h3>Deploy</h3>
+                            <p>Deploy from Dev Folder</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-rocket"></i>
+                        </div>
+                        <a href="/admin/system/deploy-from-dev" class="small-box-footer">Deploy Now <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>Update</h3>
+                            <p>System Update (Git)</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-download"></i>
+                        </div>
+                        <a href="/admin/system/update" class="small-box-footer">Check Updates <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
             </div>
         <?php endif; ?>
     </div>
